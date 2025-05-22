33f27bc3b5c5a3eeedd73937b1d44d67b31a69ba
ryochin33 <1210.ryo.i@gmail.com>
2025-05-23 07:01:45 +0900

    create route & controller

--- webapp/routes/web.php
+++ webapp/routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostsController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/index', [PostsController::class, 'index']);
+
+Route::get('/show', [PostsController::class, 'show']);
